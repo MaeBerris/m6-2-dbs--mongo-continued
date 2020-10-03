34741d0b9e1f2bf7499a119fe469992c5632946c
Mae <maeberris@outlook.com>
2020-10-03 15:46:07 -0400

    completed exercise 3

--- server/handlers.js
+++ server/handlers.js
@@ -2,6 +2,7 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
+const assert = require("assert");
 
 const options = {
   useNewUrlParser: true,
@@ -35,4 +36,49 @@ const getSeats = async (req, res) => {
   client.close();
 };
 
-module.exports = { getSeats };
+let lastBookingAttemptSucceeded = false;
+
+const bookSeat = async (req, res) => {
+  const { seatId, creditCard, expiration } = req.body;
+  console.log("seatId", seatId);
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  const db = client.db("workshop6-2");
+  const foundItem = await db.collection("Seats").findOne({ _id: seatId });
+  console.log("fundItem", foundItem);
+  if (foundItem.isBooked) {
+    return res.status(400).json({
+      message: "This seat has already been booked!",
+    });
+  }
+  if (!creditCard || !expiration) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please provide credit card information!",
+    });
+  }
+
+  if (lastBookingAttemptSucceeded) {
+    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+
+    return res.status(500).json({
+      message: "An unknown error has occurred. Please try your request again.",
+    });
+  }
+
+  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+  const newValue = { $set: { isBooked: true } };
+  const r = await db.collection("Seats").updateOne({ _id: seatId }, newValue);
+
+  assert.equal(1, r.matchedCount);
+  assert.equal(1, r.modifiedCount);
+
+  return res.status(200).json({
+    status: 200,
+    success: true,
+  });
+};
+
+module.exports = { getSeats, bookSeat };
